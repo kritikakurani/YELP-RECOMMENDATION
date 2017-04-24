287bc52c4552e3a1f54802f90cbbf20b09505792
Ye.Yuan <yyvettey@gmail.com>
2017-04-24 17:43:23 -0500

    modify parameter

--- bin/TopicMF-YY.py
+++ bin/TopicMF-YY.py
@@ -62,8 +62,7 @@ Times = 50
 DocWord = 300
 DocTopic = 5
 
-sim_val = 0.85 # default = 0.85
-vip_val = 0.20 # default = 0.20
+lbd0 = 0.5 # optimize
 
 num_user = len(user_avg)
 num_business = len(business_avg)
@@ -97,6 +96,51 @@ for i in xrange(num_user):
         if val < 1: val = 1
         if val > 5: val = 5
         Ubb[i].append(val) 
+
+UbbPd = []
+for r in xrange(num_test):
+    UbbPd.append(Ubb[test_user[r]][test_business[r]]) 
+
+#UbbPdInt = []
+#for r in UbbPd: 
+#    UbbPdInt.append(float2int(r)) 
+
+Ubb_rmse = mean_squared_error(test_rating, UbbPd)  
+#UbbPdInt_rmse = mean_squared_error(test_rating, UbbPdInt) 
+
+print Ubb_rmse
+#print UbbPdInt_rmse
+
+BasicIn = []
+for i in xrange(num_user):
+    BasicIn.append([])
+    for j in xrange(num_business):
+        val = user_avg[i] + business_avg[j] - mu
+        if val < 1: val = 1
+        if val > 5: val = 5
+        BasicIn[i].append(val) 
+        
+for r in xrange(num_train):
+    BasicIn[train_user[r]][train_business[r]] = train_rating[r]
+
+model = NMF(n_components=K_topic, init='random', random_state=0)
+U = model.fit_transform(BasicIn);
+V = model.components_;
+BasicOut = np.dot(U,V)
+
+BasicPd = []
+for r in xrange(num_test):
+    BasicPd.append(BasicOut[test_user[r]][test_business[r]]) 
+
+#BasicPdInt = []
+#for r in BasicPd: 
+#    BasicPdInt.append(float2int(r)) 
+
+BasicPd_rmse = mean_squared_error(test_rating, BasicPd)  
+#BasicPdInt_rmse = mean_squared_error(test_rating, BasicPdInt) 
+
+print BasicPd_rmse
+#print BasicPdInt_rmse
 
 Breview = [""]*num_business
 for r in xrange(num_train):
@@ -135,7 +179,7 @@ val = np.array([])
 TopicIn = csr_matrix((val,(row,col)), shape=(num_user,num_business)).toarray()
 for i in xrange(num_user):
     for j in xrange(num_business):
-        val = Ubb[i][j] + math.atan(deltaReg[i][j])
+        val = Ubb[i][j] + lbd0*deltaReg[i][j]
         if val < 1: val = 1
         if val > 5: val = 5
         TopicIn[i][j] = val
@@ -146,4 +190,9 @@ for r in xrange(num_test):
 
 TopicInPd_rmse = mean_squared_error(test_rating, TopicInPd)  
 print TopicInPd_rmse
+
+print "Ubb_rmse =", Ubb_rmse
+print "BasicPd_rmse =", BasicPd_rmse
+print "TopicInPd_rmse =", TopicInPd_rmse
+#print "SocialInPd_rmse =", SocialInPd_rmse
 
